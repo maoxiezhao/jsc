b9536e71f3ffc294d3031d557e9a18974539c46d
ZYYYYY <844635471@qq.com>
2020-09-14 19:48:59 +0800

    update

---
+++ pypi/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='jsc', 
+    version='0.1.0',
+    description='a json-like config data format',
+    author='ZZZZY',
+    author_email='zhaoyang4231@gmail.com',
+    url='https://github.com/maoxiezhao/Cjing3D',
+    python_requires='>=3.6',
+    packages=setuptools.find_packages(),
+)
